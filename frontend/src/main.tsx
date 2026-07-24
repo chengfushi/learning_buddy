dd10e09a3e787f968603fbf4ccf3043f0b45bba8
chengfushi <303623518@qq.com>
2026-07-24 15:22:21 +0800

    feat: add frontend render error boundary Refs #3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./auth";
+import ErrorBoundary from "./components/ErrorBoundary";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
